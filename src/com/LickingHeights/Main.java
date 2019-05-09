7d72a5b454a929c5294bf46969a92de9af8f8903
Hannah <hannah.bohlen.2021@student.lhschools.org>
2019-05-09 07:31:09 -0400

    Created Array Cmove

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,8 +1,59 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner keyboard;
+        keyboard = new Scanner(System.in);
+
+        boolean loop = true;
+        int a[]=new int[2];
+        //a is the game's choice play in a numeral value
+        a[0]= 0; //Rock
+        a[1]=1; //Paper
+        a[2]=2; //Scissors
+
+        String usermMdium;
+        String computerString;
+
+
+
+
+        System.out.println(" _____________________________________");
+        System.out.println("|                                     |");
+        System.out.println("|                                     |");
+        System.out.println("|                                     |");
+        System.out.println("|      _______         ________       |");
+        System.out.println("|                                     |");
+        System.out.println("|                 <                   |");
+        System.out.println("|                                     |");
+        System.out.println("|                                     |");
+        System.out.println("|                                     |");
+        System.out.println("|         _________________           |");
+        System.out.println("|                                     |");
+        System.out.println("|                                     |");
+        System.out.println("|                                     |");
+        System.out.println("|_____________________________________|");
+
+        System.out.println(" ");
+        System.out.println("You are about to go up against a highly intelligent computer in a game of Rock, Paper, Scissors.");
+
+        while (loop){
+            System.out.println("Prepare yourself. Please choose a medium to play.");
+            System.out.println("We will play best of three.");
+            System.out.println(" ");
+            usermMdium = keyboard.nextLine();
+            System.out.println(" ");
+
+
+
+
+        }
+
+
+
     }
 }

91c588a04b2823c7a3c1f5ceb9c91cd2ce611019
Hannah <hannah.bohlen.2021@student.lhschools.org>
2019-05-09 08:27:13 -0400

    Created method gameChoice and Random number generator

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,5 +1,6 @@
 package com.LickingHeights;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -9,18 +10,24 @@ public class Main {
         Scanner keyboard;
         keyboard = new Scanner(System.in);
 
-        boolean loop = true;
-        int a[]=new int[2];
+        boolean Restart = true;
+        boolean threeRounds = true;
+
+        String a[]=new String[3];
         //a is the game's choice play in a numeral value
-        a[0]= 0; //Rock
-        a[1]=1; //Paper
-        a[2]=2; //Scissors
+        a[0]= "Rock";
+        a[1]= "Paper";
+        a[2]= "Scissors";
 
-        String usermMdium;
+        int randomNumber = new Random().nextInt(a.length);
+
+        String userMedium;
         String computerString;
+        int userScore;
+        int computerScore;
+        int userChoiceNumber;
 
-
-
+        //GAME START
 
         System.out.println(" _____________________________________");
         System.out.println("|                                     |");
@@ -41,19 +48,44 @@ public class Main {
         System.out.println(" ");
         System.out.println("You are about to go up against a highly intelligent computer in a game of Rock, Paper, Scissors.");
 
-        while (loop){
-            System.out.println("Prepare yourself. Please choose a medium to play.");
-            System.out.println("We will play best of three.");
+        //while (Restart){
+        System.out.println("Prepare yourself. Please choose a medium to play.");
+        System.out.println("We will play best of three rounds.");
+        System.out.println(" ");
+
+       // while (threeRounds) {
+            userMedium = keyboard.nextLine();
+            userChoiceNumber=userNumber(userMedium);
             System.out.println(" ");
-            usermMdium = keyboard.nextLine();
+
+            System.out.println(userChoiceNumber); //DEBUG LINE
+            System.out.println(randomNumber); //DEBUG LINE
+
+            computerString = gameChoice(randomNumber);
+
+            System.out.println(" ");
+
+            System.out.println("I choose " + computerString + "!");
+
             System.out.println(" ");
 
 
+        //}
+    }
 
 
+    public static String gameChoice(int randomNumber){
+
+        String computerString;
+        if (randomNumber==0){
+            computerString = "rock";
         }
-
-
-
+        else if (randomNumber==1){
+            computerString = "paper";
+        }
+        else {
+            computerString = "scissors";
+        }
+        return computerString;
     }
 }

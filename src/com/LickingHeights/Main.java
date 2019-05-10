69176e136eb5d324ce11019e1dbb8faf4f2eb69e
Hannah <hannah.bohlen.2021@student.lhschools.org>
2019-05-10 07:19:38 -0400

    Created WinOrLose method

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -15,9 +15,9 @@ public class Main {
 
         String a[] = new String[3];
         //a is the game's choice play in a numeral value
-        a[0] = "Rock";
-        a[1] = "Paper";
-        a[2] = "Scissors";
+        a[0] = "rock";
+        a[1] = "paper";
+        a[2] = "scissors";
 
         int randomNumber = new Random().nextInt(a.length);
 
@@ -48,40 +48,45 @@ public class Main {
         System.out.println(" ");
         System.out.println("You are about to go up against a highly intelligent computer in a game of Rock, Paper, Scissors.");
 
-        while (Restart) {
+       // while (Restart) {
             System.out.println("We will play best of three rounds.");
             System.out.println("Prepare yourself. Please choose a medium to play.");
             System.out.println(" ");
 
-            while (rounds) {
+            //while (rounds) {
                 userMedium = keyboard.nextLine();
                 userChoiceNumber = userNumber(userMedium);
                 System.out.println(" ");
 
                 // System.out.println(userChoiceNumber); //DEBUG LINE
-                // System.out.println(randomNumber); //DEBUG LINE
 
-                computerString = gameChoice(randomNumber);
                 System.out.println(" ");
-                System.out.println("I choose " + computerString + "!");
+                System.out.println("I choose " + a[randomNumber] + "!");
                 System.out.println(" ");
 
 
-            }
-        }
+           // }
+      //  }
     }
 
-
-    public static String winOrLose(int randomNumber, int userChoiceNumber){
+    public static int winOrLose(int randomNumber, int userChoiceNumber){
 
         int WOL; //this is either "1", "2", or "3". This value will determine if the person won, lost, or tied.
 
         if (randomNumber==2 && userChoiceNumber==0 ||
                 randomNumber==0 && userChoiceNumber==1 ||
                     randomNumber==0 && userChoiceNumber==2){
-            WOL
+            WOL = 1; //Player wins!
         }
-
+        else if (randomNumber==0 && userChoiceNumber==2 ||
+                    randomNumber==1 && userChoiceNumber==0 ||
+                        randomNumber==2 && userChoiceNumber==1) {
+            WOL = 2; //Computer wins!
+        }
+        else {
+            WOL = 3; //User and computer tie
+        }
+        return WOL;
     }
 
 
@@ -98,5 +103,5 @@ public class Main {
         }
         return userChoiceNumber;
     }
-    
+
 }
